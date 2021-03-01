9227ccdb1e5f7d0bdea50a3bd70bdd04d58c9351
Personal <Personal@Familia>
2021-03-01 17:23:53 -0600

    NumLetras59538

---
+++ src/tiposdedatosarea59538/TiposDeDatosArea59538.java
@@ -0,0 +1,28 @@
+/*
+ * Lenguajes de programación I
+ * Facultad de Ingeniería
+ * Universidad Autónoma de Campeche
+ */
+package tiposdedatosarea59538;
+
+/**
+ *
+ * @author JossmarOswaldoKuCupul
+ */
+public class TiposDeDatosArea59538 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Lados del rectángulo
+        int lado1 = 4;
+        float lado2 = 7.5F;
+        
+        // Imprimir lados y área
+        System.out.println("primerLado = " + lado1);
+        System.out.println("segundoLado = " + lado2);
+        System.out.println("areaRectángulo = " + lado1 * lado2);
+    }
+}
